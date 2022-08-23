3c3470d23c32f0594f314c05f146874f3c192c29
juan.ramon.esteban <juan.ramon.esteban@upc.edu>
2022-08-23 18:49:25 +0200

    resolved and styled

--- src/script.js
+++ src/script.js
@@ -2,10 +2,29 @@ new Vue({
   el: '#app',
   
   data () {
-    return {}
+    return {
+      courses: [],
+      title: null,
+      time: null
+
+    }
   },
   
-  computed: {},
+  computed: {
+    totalTime() {
+      return this.courses.reduce( (total, c) => total += +c.time,0)
+    }
+  },
   
-  methods: {}
+  methods: {
+    addCourse() {
+      if ( !this.title || !this.time) return
+      this.courses.push({
+        title: this.title,time: this.time
+      })
+      this.title = null
+      this.time = null
+
+    }
+  }
 })
